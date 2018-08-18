75d7786130ddf032181a0d6126eee45eda589f14
Huan LI <zixia@zixia.net>
2018-08-18 11:17:58 +0800

    use docker image v0.20 for better experience

--- bin/docker-config.sh
+++ bin/docker-config.sh
@@ -1,9 +1,9 @@
-# WECHATY_IMAGE=zixia/wechaty:0.18
+# WECHATY_IMAGE=zixia/wechaty:0.20
 
 # Docker Registry Officla Mirror for China Users
 #   https://www.docker-cn.com/registry-mirror
 #   https://docs.docker.com/registry/recipes/mirror/#use-case-the-china-registry-mirror
-WECHATY_IMAGE=registry.docker-cn.com/zixia/wechaty:latest
+WECHATY_IMAGE=registry.docker-cn.com/zixia/wechaty:0.20
 
 function wechaty::env () {
   envText=$(env | grep WECHATY_)
